3f1c3d23e9adc5b59f8577b3535427a93dfe2c60
adib-arfan <reshad2k6@gmail.com>
2026-07-14 19:43:42 +0600

    list with prepend()

--- linked_list/linked_list.cpp
+++ linked_list/linked_list.cpp
@@ -1,48 +1,47 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 typedef struct node Node;
 
 struct node
 {
-    int data;
-    Node *next;
+   int data;
+   Node* nxt; // arekta struct node ke point kore
 };
 
-
-Node* create_node(int data, Node* nxt)
+Node* create_node(int item , Node* nxt) // notun created node take je pointer point kore seita return kore
 {
     Node* new_node = new Node();
 
-    new_node->data  = data;
-    new_node->next = nxt;
+    new_node->data = item;
+    new_node->nxt = nxt;
 
     return new_node;
 }
+
 
 Node* prepend(int item , Node* head)
 {
-    Node* new_node = create_node(item , head); // jei node ta ami pabo , seita notun data shoho notun akta node
-                                               // jeita point kore thakbe head age jake point kortesilo take
-
+    Node* new_node = create_node(item , head);
     return new_node;
 }
 
+
+
+
 int main()
 {
-    Node* n1 , *n2 , *head , *n3;
+    Node *n1 , *n2 , *head ;
 
-    n1 = create_node(10 , NULL);
-    head = n1 ;
+    n1 = create_node(10, NULL); // aita akhn prothom node
+    head = n1 ; // prothom node k point kore head
 
-    head = prepend(20 , head); // notun akta node toiri hobe , head age jake point kortesilo , aitar *next take point korbe
-                        // ar notun node take point korbe hocche head  
+    head = prepend(20 , head); // new node ta head jake point kortesilo , take point korbe
+                               // are new node ta k abr point korbe head
 
-    n2 = head ;
+    n2 = head; // first node tar pointer , head o first node takei point kore
 
-    cout << "The first data " << n2->data << endl ;
+    cout << "The first data is " << n2->data << endl ;
+    cout << "The second data is " << n1->data << endl;
 
-    n3 = n2->next;
-
-    cout << "The second data " << n3->data << endl ;
 }
